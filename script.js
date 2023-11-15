b6cd9f2ffff211682e3959593f80747890917d6e
Remy Drain <remydrain@gmail.com>
2023-11-15 17:43:11 -0500

    Add to script

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 30; i++) {
+    const colorContainer = document.createElement('div');
+    colorContainer.classList.add('color-container');
+    container.appendChild(colorContainer);
+}
+
+const colorContainers = document.querySelectorAll('.color-container');
+
+generateColors();
+
+function generateColors() {
+    colorContainers.forEach((colorContainer) => {
+        const color = randomColor();
+        colorContainer.style.backgroundColor = '#' + color;
+        colorContainer.innerText = '#' + color;
+    })
+}
+
+function randomColor() {
+    const chars = '0123456789abcdef';
+    const colorCodeLength = 6;
+    let colorCode = '';
+
+    for (let i = 0; i < colorCodeLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        colorCode += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return colorCode;
+}
